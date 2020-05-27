c8b7c2e2eb6943f11b8cf0aa4349b088f256c7dd
Bruno Saboia <brunosaboia@creactive.com.br>
2020-05-27 11:57:09 -0300

    Fix build script for BSD/MacOS (#1)
    The build script uses the `sed` command, which has a different
    implementation for GNU and for BSD/MacOS.
    
    This commit also deletes useless trailing spaces and add build files to
    .gitignore. Those files are for build purposes only, thus they do not
    need to be tracked.
    
    Please read more at:
    
    https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac

--- Linux/build.sh
+++ Linux/build.sh
@@ -7,27 +7,38 @@ DOTNET_SPARK_VERSION=0.11.0
 SPARK_VERSION=2.4.5
 SHORTVER="${SPARK_VERSION:0:3}"
 
+replace_text_with_sed()
+{
+    filename=$1
+    original_text=$2
+    final_text=$3
+
+    sh -c 'sed -i.bak "s/$1/$2/g" "$3" && rm "$3.bak"' _ "$original_text" "$final_text" $filename
+}
+
 build_dotnet-sdk-linux()
 {
     cd dotnet-sdk
     docker build --build-arg DOTNET_CORE_VERSION=$DOTNET_CORE_VERSION -t 3rdman/dotnet-sdk-linux:$DOTNET_CORE_VERSION .
     cd -
-} 
+}
 
 build_dotnet-spark-base-linux()
 {
     cd dotnet-spark
     rm -rf HelloSpark
     cp -r templates/HelloSpark ./HelloSpark
-    sed -i "s/<TargetFramework><\/TargetFramework>/<TargetFramework>netcoreapp${DOTNET_CORE_VERSION}<\/TargetFramework>/g" HelloSpark/HelloSpark.csproj
-    sed -i "s/PackageReference Include=\"Microsoft.Spark\" Version=\"\"/PackageReference Include=\"Microsoft.Spark\" Version=\"${DOTNET_SPARK_VERSION}\"/g" HelloSpark/HelloSpark.csproj
-    
-    sed -i "s/netcoreappX.X/netcoreapp${DOTNET_CORE_VERSION}/g" HelloSpark/README.txt
-    sed -i "s/spark-X.X.X/spark-${SHORTVER}.x/g" HelloSpark/README.txt
-    sed -i "s/spark-${SHORTVER}.x-X.X.X.jar/spark-${SHORTVER}.x-${DOTNET_SPARK_VERSION}.jar/g" HelloSpark/README.txt
+
+    replace_text_with_sed HelloSpark/HelloSpark.csproj "<TargetFramework><\/TargetFramework>" "<TargetFramework>netcoreapp${DOTNET_CORE_VERSION}<\/TargetFramework>"
+    replace_text_with_sed HelloSpark/HelloSpark.csproj "PackageReference Include=\"Microsoft.Spark\" Version=\"\"" "PackageReference Include=\"Microsoft.Spark\" Version=\"${DOTNET_SPARK_VERSION}\""
+
+    replace_text_with_sed HelloSpark/README.txt "netcoreappX.X" "netcoreapp${DOTNET_CORE_VERSION}"
+    replace_text_with_sed HelloSpark/README.txt "spark-X.X.X" "spark-${SHORTVER}.x"
+    replace_text_with_sed HelloSpark/README.txt "spark-${SHORTVER}.x-X.X.X.jar" "spark-${SHORTVER}.x-${DOTNET_SPARK_VERSION}.jar"
+
     docker build --build-arg DOTNET_SPARK_VERSION=$DOTNET_SPARK_VERSION -t 3rdman/dotnet-spark:$DOTNET_SPARK_VERSION-base-linux .
     cd -
-} 
+}
 
 build_dotnet-spark-linux()
 {
@@ -35,11 +46,11 @@ build_dotnet-spark-linux()
     rm -rf sbin
     cp -r templates/sbin ./sbin
 
-    sed -i "s/microsoft-spark-X.X.X/microsoft-spark-${SHORTVER}.x/g" sbin/start-spark-debug.sh
+    replace_text_with_sed sbin/start-spark-debug.sh "microsoft-spark-X.X.X" "microsoft-spark-${SHORTVER}.x"
     docker build --build-arg DOTNET_SPARK_VERSION=$DOTNET_SPARK_VERSION --build-arg SPARK_VERSION=$SPARK_VERSION -t 3rdman/dotnet-spark:$DOTNET_SPARK_VERSION-spark-$SPARK_VERSION-linux .
     cd -
 }
- 
+
 build_dotnet-sdk-linux
 build_dotnet-spark-base-linux
 build_dotnet-spark-linux
