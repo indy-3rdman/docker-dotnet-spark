0c5df4ef6883dacc31e06c37fe74c70ba35237c7
indy <indy@3rdman.de>
2021-03-31 08:10:41 +0200

    update to 1.1.1

--- images/interactive/build.sh
+++ images/interactive/build.sh
@@ -13,10 +13,10 @@ readonly supported_apache_spark_versions=(
     "2.4.0" "2.4.1" "2.4.3" "2.4.4" "2.4.5" "2.4.6" "2.4.7"
     "3.0.0" "3.0.1"
     )
-readonly supported_dotnet_spark_versions=("1.0.0" "1.0.1")
+readonly supported_dotnet_spark_versions=("1.0.0" "1.1.1")
 readonly dotnet_core_version=3.1
 
-dotnet_spark_version=1.0.1
+dotnet_spark_version=1.1.1
 dotnet_spark_jar=""
 apache_spark_version=3.0.1
 apache_spark_short_version="${apache_spark_version:0:3}"
